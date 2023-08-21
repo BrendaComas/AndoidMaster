15fe7e70aed124910509350b42d11cb3a446b362
BrendaComas <brendacomasok@gmail.com>
2023-08-21 12:18:49 -0300

    agregue la clase calculator

--- app/src/main/java/com/curso/androidmaster/MenuActivity.kt
+++ app/src/main/java/com/curso/androidmaster/MenuActivity.kt
@@ -5,15 +5,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.curso.androidmaster.firstapp.FirstAppActivity
+import com.curso.androidmaster.imcCalculator.ImcCalculatorActivity
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
-            val btnSaludApp =findViewById<Button>(R.id.btnSaludApp)
-        btnSaludApp.setOnClickListener{ navigateToSaludApp()}
+        val btnSaludApp = findViewById<Button>(R.id.btnSaludApp)
+        val btnImcApp = findViewById<Button>(R.id.btnIMCApp)
+
+        btnSaludApp.setOnClickListener { navigateToSaludApp() }
+        btnImcApp.setOnClickListener { navigateToImcApp() }
     }
-   private fun navigateToSaludApp(){
+
+    private fun navigateToImcApp() {
+        val intent = Intent(this, ImcCalculatorActivity::class.java )
+        startActivity(intent)
+    }
+
+    private fun navigateToSaludApp() {
         val intent = Intent(this, FirstAppActivity::class.java)
         startActivity(intent)
     }
